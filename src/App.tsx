f4632afa9234ba691c600572ac6c34fc774c2368
samirzjadhav <samirzjadhav@gmail.com>
2025-10-14 13:52:19 +0530

    disc: added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      {" "}
+      <div className="flex h-screen items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600 text-white text-4xl font-bold">
+        Tailwind v3.4.17 + TypeScript + Vite ⚡
+      </div>
+    </div>
+  );
 };
 
 export default App;

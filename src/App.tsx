d228264e3b3cb4d1af9fffabdaf87ac9ce12c192
samirzjadhav <samirzjadhav@gmail.com>
2025-10-16 19:49:55 +0530

    feat: added new style on tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 const App = () => {
   return (
-    <div className="app">
-      {" "}
-      <div className="flex h-screen items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600 text-white text-4xl font-bold">
-        Tailwind v3.4.17 + TypeScript + Vite ⚡
-      </div>
+    <div className="app bg-gray-20">
+    <h1 className="">hddj</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,37 +3,35 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      color: {
-        "gray-20" : "#f8f4eb",
-        "gray-50" : "#efe6e6",
-        "gray-100" : "#dfcccc",
-        "gray-500" : "#5e0000",
-        "primary-100" : "#ffe1e0",
-        "primary-300" : "#ffa6a3 ",
-        "primary-500" : "#ff6b66",
-        "secondary-400" : "#ffcd58",
-        "secondary-500" : "#ffc132",
+      colors: {
+        "gray-20": "#f8f4eb",
+        "gray-50": "#efe6e6",
+        "gray-100": "#dfcccc",
+        "gray-500": "#5e0000",
+        "primary-100": "#ffe1e0",
+        "primary-300": "#ffa6a3",
+        "primary-500": "#ff6b66",
+        "secondary-400": "#ffcd58",
+        "secondary-500": "#ffc132",
       },
-      backgroundImage: (theme) =>({
-        'gradient-yellowred' : 'linear-gradient(90deg , #ff616a 0%, #ffc837 100%)',
-        'mobile-home' : "url('./assets/HomePageGraphic.png')",
-      }),
-      fontFamily : {
+      backgroundImage: {
+        'gradient-yellowred': 'linear-gradient(90deg, #ff616a 0%, #ffc837 100%)',
+        'mobile-home': "url('./assets/HomePageGraphic.png')",
+        'evolvetext': "url('./assets/EvolveText.png')",
+        'abstractwaves': "url('./assets/AbstractWaves.png')",
+        'sparkles': "url('./assets/Sparkles.png')",
+        'circles': "url('./assets/Circles.png')",
+      },
+      fontFamily: {
         dmsans: ["DM Sans", 'sans-serif'],
-        montserrat: ['Montserrat','sans-serif']
+        montserrat: ['Montserrat', 'sans-serif']
       },
-      content:{
-        evolvetext: "url('./assets/EvolveText.png')",
-        abstractwaves: "url('./assets/AbstractWaves.png')",
-        sparkles: "url('./assets/Sparkles.png')",
-        cirlces: "url('./assets/circles.png')",
-      }
     },
     screens: {
       xs: "480px",
       sm: "768px",
-      md: "1060px"
-    }
+      md: "1060px",
+    },
   },
   plugins: [],
 };

a32a025aec4b272be91b6067ac74f924dcfde3d5
samirzjadhav <samirzjadhav@gmail.com>
2026-03-31 10:00:00 +0530

    feat: added top page function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Navbar from '@/scenes/navbar'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 enum SelectedPage {
   Home = 'home',
@@ -9,7 +9,18 @@ enum SelectedPage {
 }
 
 const App = () => {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(()=>{
+    const handleScroll = ()=>{
+      if(window.scrollY === 0){
+        setIsTopOfPage(true)
+      }
+    }
+  })
+
+
   return (
     <div className="app bg-gray-20 ">
      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
